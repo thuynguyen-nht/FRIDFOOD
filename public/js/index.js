0b5e56d52b83411207287a3578102c662aedae29
ThuyN <thuynguyen.nht20@gmail.com>
2020-05-06 16:19:17 -0700

    add log pages and inventory pages

--- public/js/index.js
+++ public/js/index.js
@@ -28,4 +28,86 @@ $(document).ready(() => {
       }
     ]
   });
+
+  $(function() {
+    $("#signUp").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+      if (
+        !$("#firstName")
+          .val()
+          .trim() ||
+        !$("#lastName")
+          .val()
+          .trim() ||
+        !$("#inputEmail")
+          .val()
+          .trim() ||
+        $("#inputPassword")
+          .val()
+          .trim()
+      ) {
+        return;
+      }
+
+      var newUser = {
+        firstName: $("#firstName")
+          .val()
+          .trim(),
+        lastName: $("#lastName")
+          .val()
+          .trim(),
+        email: $("#inputEmail")
+          .val()
+          .trim(),
+        password: $("#inputPassword")
+          .val()
+          .trim()
+      };
+      console.log(newUser);
+
+      // Send the POST request.
+      $.ajax("_________", {
+        type: "POST",
+        data: newUser
+      }).then(function() {
+        console.log("created new user");
+        // Reload the page to get the updated list
+        window.location.href = "/mainPage";
+      });
+    });
+
+    $("#logIn").on("click", function() {
+      event.preventDefault();
+      var id = $(this).data("id");
+
+      // Send the DELETE request.
+      $.ajax("/____" + id, {
+        type: "GET"
+      }).then(function() {
+        console.log("Welcome back, ", id);
+        // Reload the page to get the updated list
+        window.location.href = "/mainPage";
+      });
+    });
+
+    $("#logIn").on("click", function() {
+      window.location.href = "/index";
+    });
+  });
+
+  $("#search").keyup(function() {
+    $("#result").html("");
+    var searchField = $("#search").val();
+    var expression = new RegExp(searchField, "i");
+    $.getJSON("______", function(data) {
+      $.each(data, function(key, value) {
+        if (value.name.search(expression) !== -1) {
+          $("#result").append(
+            "<li class='list-group-item'>" + value.name + "</li>"
+          );
+        }
+      });
+    });
+  });
 });
