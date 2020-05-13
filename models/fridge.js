6b2fe23eb9a2bc66adf5de877252b77426d8ba05
ThuyN <thuynguyen.nht20@gmail.com>
2020-05-13 16:28:00 -0700

    fix master

--- models/fridge.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-  var Fridge = sequelize.define("Fridge", {
-    UserId: DataTypes.STRING,
-    ingredientName: DataTypes.TEXT
-  });
-  return Fridge;
-};
--- models/user.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define("User", {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    UserId: DataTypes.STRING
-  });
-  return User;
-};
